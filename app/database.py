1fc93f89cee2776f0cb8daaeb9234cc921d786d4
Matthew Slipenchuk <mslipenchuk267@gmail.com>
2024-05-05 23:04:09 -0400

    Dockerized and added dev and prod compose yaml

--- app/database.py
+++ app/database.py
@@ -2,7 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypass@localhost/fastapitutdb"
+import os
+
+SQLALCHEMY_DATABASE_HOST = os.getenv("FASTAPITUT_POSTGRESQL_DB_HOST")
+SQLALCHEMY_DATABASE_NAME = os.getenv("FASTAPITUT_POSTGRESQL_DB_NAME")
+SQLALCHEMY_DATABASE_USER = os.getenv("FASTAPITUT_POSTGRESQL_DB_USER")
+SQLALCHEMY_DATABASE_PASS = os.getenv("FASTAPITUT_POSTGRESQL_DB_PASS")
+
+#SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypass@localhost/fastapitutdb"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{SQLALCHEMY_DATABASE_USER}:{SQLALCHEMY_DATABASE_PASS}@{SQLALCHEMY_DATABASE_HOST}/{SQLALCHEMY_DATABASE_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
